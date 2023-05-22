72b2d39e280d15846406ce4d50b27d6b0112f845
Gangneng <khak1998@naver.com>
2023-05-22 15:51:18 +0900

    Add insert function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,21 @@
 package org.example;
 
-import java.sql.ResultSet;
-
-import static org.example.Connection.conn;
-import static org.example.Connection.st;
+import org.example.service.BookWriteService;
 
 public class Main {
 
     public static void main(String[] args) {
-        connectDB();
         init();
-        while (true){
+        BookWriteService bookWriteService = BookWriteService.getInstance();
 
+        while (true){
+            bookWriteService.getNewBookInfo();
+            break;
         }
     }
 
     private static void init() {
         System.out.println("반갑습니다");
         System.out.println("");
-    }
-
-    private static void connectDB() {
-        try{
-            ResultSet rs = st.executeQuery("show databases");
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally{
-            try{
-                if(st!=null)
-                    st.close();
-                if(conn !=null)
-                    conn.close();
-            }catch(Exception e2){
-                e2.printStackTrace();
-            }
-        }
     }
 }
--- src/main/java/org/example/repository/BookRepository.java
+++ src/main/java/org/example/repository/BookRepository.java
@@ -4,7 +4,6 @@ import org.example.entity.Book;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
 
 
@@ -14,15 +13,32 @@ public class BookRepository {
     private Statement st;
     private BookRepository(){
         try{
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db01?serverTimezon=UTC","root","1234");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library?serverTimezon=UTC","root","1234");
             st = con.createStatement();
             System.out.println("데이터 베이스와 연결이 성공적으로 되었습니다");
         }catch(Exception e){
             e.printStackTrace();
         }
     }
-    public BookRepository getInstance(){
+    public static BookRepository getInstance(){
         return bookRepository;
+    }
+
+    public void insertBook(Book book){
+        try{
+            st.execute("insert into books(book_id, bookname, author, publisher, borrowcount, ISBN_NO, year_of_publication, count) values (null,'"+book.getBook_name()+"','"+book.getAuthor()+"','"+book.getPublisher()+"','"+book.getBorrow_count()+"','"+book.getISBN_NO()+"','"+book.getYear_of_publication()+"','"+book.getCount()+"');");
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(st!=null)
+                    st.close();
+                if(con !=null)
+                    con.close();
+            }catch(Exception e2){
+                e2.printStackTrace();
+            }
+        }
     }
 
 }
