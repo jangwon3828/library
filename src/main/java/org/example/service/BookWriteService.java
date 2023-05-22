062997b67796b9e7140c55d46db07ae4abafb6cf
장원진 <wkd3828@gmail.com>
2023-05-22 16:48:25 +0900

    살짝 수정

--- src/main/java/org/example/service/BookWriteService.java
+++ src/main/java/org/example/service/BookWriteService.java
@@ -7,6 +7,9 @@ import org.example.repository.BookRepository;
 
 import java.time.LocalDate;
 
+import static org.example.input.InputView.inputLocalDateTime;
+import static org.example.input.InputView.inputString;
+
 public class BookWriteService {
     private static BookWriteService bookWriteService=new BookWriteService();
     private BookWriteService(){
@@ -17,12 +20,12 @@ public class BookWriteService {
     }
 
     public void getNewBookInfo(){
-        String bookName = InputView.inputString("책 제목을 입력해 주세요.");
-        String author = InputView.inputString("저자명을 입력해 주세요.");
-        String publisher = InputView.inputString("출판사를 입력해 주세요.");
+        String bookName = inputString("책 제목을 입력해 주세요.");
+        String author = inputString("저자명을 입력해 주세요.");
+        String publisher = inputString("출판사를 입력해 주세요.");
         Long borrowCount = InputView.inputLong("해당 책의 누적 대출 횟수를 입력해 주세요.");
-        String ISBN_NO = InputView.inputString("해당 책의 ISBN_NO 값을 입력해 주세요.");
-        LocalDate year_of_publication = InputView.inputLocalDateTime("책 출간년도를 입력해 주세요. (format : yyyy-MM-dd)");
+        String ISBN_NO = inputString("해당 책의 ISBN_NO 값을 입력해 주세요.");
+        LocalDate year_of_publication = inputLocalDateTime("책 출간년도를 입력해 주세요. (format : yyyy-MM-dd)");
         Long count = InputView.inputLong("현재 도서관에 있는 책 권수를 입력해 주세요.");
 
         Book book = new Book(null, bookName, author, publisher, borrowCount, ISBN_NO, year_of_publication, count);
